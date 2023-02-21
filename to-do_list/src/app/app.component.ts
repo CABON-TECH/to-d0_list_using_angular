7e7cf03fdf7c304ea7ae153e22faabf9fcc79133
CABON-TECH <kishushubonifacembogho@gmail.com>
2023-02-21 20:21:48 +0300

    updated

--- to-do_list/src/app/app.component.ts
+++ to-do_list/src/app/app.component.ts
@@ -6,7 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'to-do_list';
+  public items = [];
+  public newTask;
+
+  public addTo
 }
 
 
